2e5c494d5dbcf5298970a2f9c94811e01cfd6cc8
Gui <guitrentinic@gmail.com>
2026-07-12 23:09:33 +0200

    working pickups

--- player/player.gd
+++ player/player.gd
@@ -17,16 +17,22 @@ var camera_base_y := STAND_CAM_Y
 var camera_target_y := STAND_CAM_Y
 var is_crouching := false
 
+var current_pickup : Node3D = null
+var inventory : Array[Ids.OBJECTS] = []
+
 @export var set_camera_as_active : bool = true
 
 @onready var col_shape: CollisionShape3D = $CollisionShape3D
+@onready var pickup_prompt: Label = $PickupPrompt
+@onready var inventory_box: HBoxContainer = $Inventory
 
 func _ready() -> void:
 	Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
 	camera_base_y = %Camera3D.position.y
 	camera_target_y = camera_base_y
-	$ItemPickup.area_entered.connect(on_pickup_area_entered)
-	
+	$ItemPickup.area_entered.connect(_on_pickup_area_entered)
+	$ItemPickup.area_exited.connect(_on_pickup_area_exited)
+
 	if set_camera_as_active:
 		%Camera3D.make_current()
 
@@ -42,6 +48,8 @@ func _unhandled_input(event: InputEvent) -> void:
 			_try_stand()
 		else:
 			_crouch()
+	elif event.is_action_pressed("interact") and current_pickup:
+		_collect_current_pickup()
 
 func _crouch() -> void:
 	is_crouching = true
@@ -94,6 +102,46 @@ func _update_headbob(delta: float) -> void:
 		bob_time = 0.0
 		%Camera3D.position.y = lerpf(%Camera3D.position.y, camera_base_y, delta * 10.0)
 
-func on_pickup_area_entered(area):
-	#inventory.append(area.get_parent().id)
-	print(area.get_parent().object_name)
+func _on_pickup_area_entered(area: Area3D) -> void:
+	var pickup = area.get_parent()
+	if not pickup.has_method("pick_up") or pickup.picked_up:
+		return
+	current_pickup = pickup
+	pickup_prompt.text = "Press F to pickup %s" % Ids.get_object_name(pickup.id)
+	pickup_prompt.visible = true
+
+func _on_pickup_area_exited(area: Area3D) -> void:
+	var pickup = area.get_parent()
+	if pickup == current_pickup:
+		current_pickup = null
+		pickup_prompt.visible = false
+
+func _collect_current_pickup() -> void:
+	var pickup = current_pickup
+	current_pickup = null
+	pickup_prompt.visible = false
+
+	pickup.pick_up()
+	inventory.append(pickup.id)
+	_add_inventory_slot(pickup)
+
+func _add_inventory_slot(pickup) -> void:
+	var slot := PanelContainer.new()
+	slot.custom_minimum_size = Vector2(64, 64)
+	slot.tooltip_text = Ids.get_object_name(pickup.id)
+
+	if pickup.icon:
+		var icon_rect := TextureRect.new()
+		icon_rect.texture = pickup.icon
+		icon_rect.custom_minimum_size = Vector2(64, 64)
+		icon_rect.stretch_mode = TextureRect.STRETCH_KEEP_ASPECT_CENTERED
+		slot.add_child(icon_rect)
+	else:
+		var label := Label.new()
+		label.text = Ids.get_object_name(pickup.id)
+		label.horizontal_alignment = HORIZONTAL_ALIGNMENT_CENTER
+		label.vertical_alignment = VERTICAL_ALIGNMENT_CENTER
+		label.autowrap_mode = TextServer.AUTOWRAP_WORD
+		slot.add_child(label)
+
+	inventory_box.add_child(slot)
